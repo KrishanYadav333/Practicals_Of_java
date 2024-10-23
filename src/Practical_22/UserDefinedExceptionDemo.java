348602a1c65618ee4287969263599c25cb0c795a
KrishanYadav333 <132331252+KrishanYadav333@users.noreply.github.com>
2024-10-23 23:59:05 +0530

    UserDefinedExceptionDemo.java

--- src/Practical_22/UserDefinedExceptionDemo.java
+++ src/Practical_22/UserDefinedExceptionDemo.java
@@ -1,4 +1,26 @@
 package Practical_22;
 
-public class UserDefinedExceptionDemo {
+//22. write a program to create user defined exception
+
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
 }
+
+public class UserDefinedExceptionDemo {
+    public static void main(String[] args) {
+        int age = 15;  // Test value
+
+        try {
+            if (age < 18) {
+                throw new InvalidAgeException("Age must be 18 or above to vote.");
+            } else {
+                System.out.println("Valid age for voting.");
+            }
+        } catch (InvalidAgeException e) {
+            System.out.println("Caught Exception: " + e.getMessage());
+        }
+    }
+}
+
