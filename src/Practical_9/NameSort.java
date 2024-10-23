2a03e06c5b275d79fb9c2bbf3c5f439e5c32cdd9
KrishanYadav333 <132331252+KrishanYadav333@users.noreply.github.com>
2024-10-23 23:52:33 +0530

    NameSort.java

--- src/Practical_9/NameSort.java
+++ src/Practical_9/NameSort.java
@@ -1,4 +1,29 @@
 package Practical_9;
 
+//9. Write a Java program for sorting a given list of names in ascending order?
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class NameSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of names: ");
+        int n = sc.nextInt();
+        sc.nextLine();  // Consume newline
+
+        String[] names = new String[n];
+        System.out.println("Enter the names:");
+        for (int i = 0; i < n; i++) {
+            names[i] = sc.nextLine();
+        }
+
+        Arrays.sort(names);
+
+        System.out.println("Sorted names in ascending order:");
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
 }
+
