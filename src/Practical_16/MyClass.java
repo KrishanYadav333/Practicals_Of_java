1f46257bf8ec9c52a0a3e060b0a549b7e23a91a9
KrishanYadav333 <132331252+KrishanYadav333@users.noreply.github.com>
2024-10-23 23:55:36 +0530

    MyClass.java

--- src/Practical_16/MyClass.java
+++ src/Practical_16/MyClass.java
@@ -1,4 +1,10 @@
 package Practical_16;
 
+//16. Write a java program to create user defined package.
+
 public class MyClass {
+    public void display() {
+        System.out.println("This is a user-defined package example.");
+    }
 }
+
