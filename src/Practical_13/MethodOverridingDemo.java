6558092e917debf1dbaf5ae1a0c8c7ae15a1130f
KrishanYadav333 <132331252+KrishanYadav333@users.noreply.github.com>
2024-10-23 23:54:12 +0530

    MethodOverridingDemo.java

--- src/Practical_13/MethodOverridingDemo.java
+++ src/Practical_13/MethodOverridingDemo.java
@@ -1,4 +1,24 @@
 package Practical_13;
 
-public class MethodOverridingDemo {
+//13. write program to demonstrate method overriding and super keyword.
+class Parent {
+    public void display() {
+        System.out.println("This is the parent class.");
+    }
 }
+
+class Child extends Parent {
+    @Override
+    public void display() {
+        super.display();  // Call the parent class method
+        System.out.println("This is the child class.");
+    }
+}
+
+public class MethodOverridingDemo {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.display();
+    }
+}
+
