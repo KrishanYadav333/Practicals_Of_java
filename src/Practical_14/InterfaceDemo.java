d2379e651cb1102d238d0d4e7dd67fc3374857a6
KrishanYadav333 <132331252+KrishanYadav333@users.noreply.github.com>
2024-10-23 23:54:35 +0530

    InterfaceDemo.java

--- src/Practical_14/InterfaceDemo.java
+++ src/Practical_14/InterfaceDemo.java
@@ -1,4 +1,27 @@
 package Practical_14;
 
-public class InterfaceDemo {
+//14. Write a java program to implement Interface using extends keyword.
+
+interface Printable {
+    void print();
+}
+
+interface Showable extends Printable {
+    void show();
+}
+
+class InterfaceDemo implements Showable {
+    public void print() {
+        System.out.println("Printing...");
+    }
+
+    public void show() {
+        System.out.println("Showing...");
+    }
+
+    public static void main(String[] args) {
+        InterfaceDemo obj = new InterfaceDemo();
+        obj.print();
+        obj.show();
+    }
 }
