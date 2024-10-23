ad034facd9d218e594ee966709dfd58a5625b196
KrishanYadav333 <132331252+KrishanYadav333@users.noreply.github.com>
2024-10-23 23:58:38 +0530

    MultipleTryCatchFinally.java

--- src/Practical_21/MultipleTryCatchFinally.java
+++ src/Practical_21/MultipleTryCatchFinally.java
@@ -1,4 +1,26 @@
 package Practical_21;
 
+//21. write a program for multiple try blocks and multiple catch blocks including finally.
+
 public class MultipleTryCatchFinally {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Inside first try block");
+            int a = 10 / 0;  // Will throw ArithmeticException
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmetic Exception caught in first block");
+        } finally {
+            System.out.println("Finally block for first try block");
+        }
+
+        try {
+            System.out.println("Inside second try block");
+            int[] arr = new int[2];
+            arr[5] = 10;  // Will throw ArrayIndexOutOfBoundsException
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array Index Out of Bounds Exception caught in second block");
+        } finally {
+            System.out.println("Finally block for second try block");
+        }
+    }
 }
