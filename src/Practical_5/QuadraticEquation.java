a5222c6bb77d80073b077502cc7f10a729c525aa
KrishanYadav333 <132331252+KrishanYadav333@users.noreply.github.com>
2024-10-23 23:50:38 +0530

    QuadraticEquation.java

--- src/Practical_5/QuadraticEquation.java
+++ src/Practical_5/QuadraticEquation.java
@@ -1,4 +1,32 @@
 package Practical_5;
 
+//5. Write a Java program that prints allrealsolutionsto the quadratic equation ax2+bx+c = 0. Read in a, b, c and use the quadratic
+//formula. If the discriminate b2-4ac is negative, display a message stating that there are no real solutions?
+
+import java.util.Scanner;
+
 public class QuadraticEquation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
+        double a = sc.nextDouble();
+        System.out.print("Enter b: ");
+        double b = sc.nextDouble();
+        System.out.print("Enter c: ");
+        double c = sc.nextDouble();
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant > 0) {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            System.out.println("Roots are: " + root1 + " and " + root2);
+        } else if (discriminant == 0) {
+            double root = -b / (2 * a);
+            System.out.println("Root is: " + root);
+        } else {
+            System.out.println("No real solutions exist.");
+        }
+    }
 }
