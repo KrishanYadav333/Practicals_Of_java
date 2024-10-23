615264b67c82620ff35bb9317accde48d6c7282d
KrishanYadav333 <132331252+KrishanYadav333@users.noreply.github.com>
2024-10-23 23:55:01 +0530

    InnerClassDemo.java

--- src/Practical_15/InnerClassDemo.java
+++ src/Practical_15/InnerClassDemo.java
@@ -1,4 +1,26 @@
 package Practical_15;
 
-public class InnerClassDemo {
+//15. Write a java program to create inner classes.
+
+class OuterClass {
+    private int outerValue = 10;
+
+    class InnerClass {
+        public void displayInner() {
+            System.out.println("Value from outer class: " + outerValue);
+        }
+    }
+
+    public void createInner() {
+        InnerClass inner = new InnerClass();
+        inner.displayInner();
+    }
 }
+
+public class InnerClassDemo {
+    public static void main(String[] args) {
+        OuterClass outer = new OuterClass();
+        outer.createInner();
+    }
+}
+
