72d4de5a9b15e9ce6821096f75885469d2ee68ce
KrishanYadav333 <132331252+KrishanYadav333@users.noreply.github.com>
2024-10-23 23:53:10 +0530

    AbstractClassDemo.java

--- src/Practical_11/AbstractClassDemo.java
+++ src/Practical_11/AbstractClassDemo.java
@@ -1,4 +1,27 @@
 package Practical_11;
 
-public class AbstractClassDemo {
+//11. Write a java program to represent Abstract class with example.
+
+abstract class Animal {
+    public abstract void sound();  // Abstract method
+
+    public void sleep() {
+        System.out.println("This animal is sleeping.");
+    }
 }
+
+class Dog extends Animal {
+    @Override
+    public void sound() {
+        System.out.println("Dog barks.");
+    }
+}
+
+public class AbstractClassDemo {
+    public static void main(String[] args) {
+        Animal myDog = new Dog();
+        myDog.sound();
+        myDog.sleep();
+    }
+}
+
