c729a63f36cd683f71e8a5a6dd13fd27456e5e5a
KrishanYadav333 <132331252+KrishanYadav333@users.noreply.github.com>
2024-10-23 23:56:55 +0530

    PalindromeCheck.java

--- src/Practical_18/PalindromeCheck.java
+++ src/Practical_18/PalindromeCheck.java
@@ -1,4 +1,21 @@
 package Practical_18;
 
+//18. Write a Java program that checks whether a given string is a palindrome or not. Ex: MADAM is a palindrome?
+
+import java.util.Scanner;
+
 public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String reversedStr = new StringBuilder(str).reverse().toString();
+        if (str.equalsIgnoreCase(reversedStr)) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+    }
 }
+
