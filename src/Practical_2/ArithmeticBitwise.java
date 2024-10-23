61b5c925e256e6d1742fca0976c9f469cba636f4
KrishanYadav333 <132331252+KrishanYadav333@users.noreply.github.com>
2024-10-23 23:47:04 +0530

    Update ArithmeticBitwise.java

--- src/Practical_2/ArithmeticBitwise.java
+++ src/Practical_2/ArithmeticBitwise.java
@@ -1,4 +1,20 @@
 package Practical_2;
+//2. Write a program to perform arithmetic and bitwise operations in a single source program without object creation.
 
 public class ArithmeticBitwise {
+    public static void main(String[] args) {
+        int a = 5, b = 3;
+
+        // Arithmetic Operations
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+
+        // Bitwise Operations
+        System.out.println("Bitwise AND: " + (a & b));
+        System.out.println("Bitwise OR: " + (a | b));
+        System.out.println("Bitwise XOR: " + (a ^ b));
+        System.out.println("Bitwise Complement of a: " + (~a));
+    }
 }
