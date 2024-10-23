166455fe681f83667038212291c014ccae6d7109
KrishanYadav333 <132331252+KrishanYadav333@users.noreply.github.com>
2024-10-23 23:58:07 +0530

    MultipleCatchDemo.java

--- src/Practical_20/MultipleCatchDemo.java
+++ src/Practical_20/MultipleCatchDemo.java
@@ -1,4 +1,19 @@
 package Practical_20;
 
+//20. Write a java program for creating single try block with multiple catch blocks.
+
 public class MultipleCatchDemo {
+    public static void main(String[] args) {
+        try {
+            int a = 5 / 0;  // ArithmeticException
+            int[] arr = new int[3];
+            arr[5] = 10;  // ArrayIndexOutOfBoundsException
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmetic Exception: " + e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array Index Out of Bounds: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("General Exception: " + e.getMessage());
+        }
+    }
 }
