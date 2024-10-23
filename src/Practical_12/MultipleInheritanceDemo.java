8a61ebe233aa98c990c5989d92a3e5fa6726d564
KrishanYadav333 <132331252+KrishanYadav333@users.noreply.github.com>
2024-10-23 23:53:47 +0530

    MultipleInheritanceDemo.java

--- src/Practical_12/MultipleInheritanceDemo.java
+++ src/Practical_12/MultipleInheritanceDemo.java
@@ -1,4 +1,27 @@
 package Practical_12;
 
-public class MultipleInheritanceDemo {
+//12. Write a program to implement multiple Inheritances.
+
+interface A {
+    public void displayA();
+}
+
+interface B {
+    public void displayB();
+}
+
+class MultipleInheritanceDemo implements A, B {
+    public void displayA() {
+        System.out.println("Displaying from interface A.");
+    }
+
+    public void displayB() {
+        System.out.println("Displaying from interface B.");
+    }
+
+    public static void main(String[] args) {
+        MultipleInheritanceDemo demo = new MultipleInheritanceDemo();
+        demo.displayA();
+        demo.displayB();
+    }
 }
